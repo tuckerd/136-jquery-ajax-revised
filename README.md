252021a4665f5936896df5cd14d1a428d2091095
D Tucker <danielle@tuckerlabs.com>
2013-08-13 15:07:16 -0700

    Ajax added to page based on walkthrough of Railscast #136

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# RailsCasts Episode #136: jQuery & Ajax (revised)
+# Workthrough of RailsCasts Episode #136: jQuery & Ajax (revised)
 
 http://railscasts.com/episodes/136-jquery-ajax-revised
 
 Requires Ruby 1.9.2 or higher.
+
+Within the directory structure you want to visit checklist-before
+for the completed walkthrough.
--- checklist-before/app/controllers/tasks_controller.rb
+++ checklist-before/app/controllers/tasks_controller.rb
@@ -11,17 +11,26 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.create!(params[:task])
-    redirect_to tasks_url
+    respond_to do |format|
+      format.html { redirect_to tasks_url }
+      format.js
+    end
   end
 
   def update
     @task = Task.find(params[:id])
     @task.update_attributes!(params[:task])
-    redirect_to tasks_url
+    respond_to do |format|
+      format.html { redirect_to tasks_url }
+      format.js
+    end
   end
 
   def destroy
     @task = Task.destroy(params[:id])
-    redirect_to tasks_url
+    respond_to do |format|
+      format.html { redirect_to tasks_url }
+      format.js
+    end
   end
 end
